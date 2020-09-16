4d488251d5fd08c079bf923e493b4dc77c6ee3be
rebecca <revip.pop@gmail.com>
2020-09-16 20:35:48 -0300

    mapeamento feito

---
+++ Models/TbListaFofa.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ef_async.Models
+{
+    [Table("tb_lista_fofa")]
+    public partial class TbListaFofa
+    {
+        [Key]
+        [Column("id_lista_fofa", TypeName = "int(11)")]
+        public int IdListaFofa { get; set; }
+        [Required]
+        [Column("nm_fofura", TypeName = "varchar(100)")]
+        public string NmFofura { get; set; }
+        [Required]
+        [Column("ds_porque", TypeName = "varchar(200)")]
+        public string DsPorque { get; set; }
+        [Column("bt_colocaria_potinho")]
+        public bool? BtColocariaPotinho { get; set; }
+        [Column("dt_niver", TypeName = "date")]
+        public DateTime DtNiver { get; set; }
+    }
+}
---
+++ Models/lfdbContext.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace ef_async.Models
+{
+    public partial class lfdbContext : DbContext
+    {
+        public lfdbContext()
+        {
+        }
+
+        public lfdbContext(DbContextOptions<lfdbContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<TbListaFofa> TbListaFofa { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
+                optionsBuilder.UseMySql("server=localhost;user id=root;password=1234;database=lfdb", x => x.ServerVersion("5.7.30-mysql"));
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<TbListaFofa>(entity =>
+            {
+                entity.HasKey(e => e.IdListaFofa)
+                    .HasName("PRIMARY");
+
+                entity.Property(e => e.DsPorque)
+                    .HasCharSet("latin1")
+                    .HasCollation("latin1_swedish_ci");
+
+                entity.Property(e => e.NmFofura)
+                    .HasCharSet("latin1")
+                    .HasCollation("latin1_swedish_ci");
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
